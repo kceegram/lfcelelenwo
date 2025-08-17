4cd88899d4939aaa4c4ac5d3d6639026ec776c1c
kceegram <158181757+kceegram@users.noreply.github.com>
2025-08-17 06:37:13 +0100

    yes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className='w-full bg-tranparent bg-opacity-2 backdrop-blur-md fixed bottom-0 left-0 right-0 z-10 border-t border-white border-opacity-30'>
       <div className='container mx-auto flex justify-center items-center h-16 sm:h-16'>
         <p className='text-sm sm:text-sm md:text-sn text-white font-meduim text-center drop-shadow-lg'>
-         LFCELELENWO © {currentYear} All Rights Reserved - Developed by Kceegram_ThinkGlobal
+         LFCELELENWO © {currentYear} All Rights Reserved - Developed by Kceegram
         </p>
       </div>
     </footer>
